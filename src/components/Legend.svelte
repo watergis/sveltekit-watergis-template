1b8593ea102bddbb80e0c41ec8245863b593cce8
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-18 21:00:48 +0100

    added legend symbol

--- src/components/Legend.svelte
+++ src/components/Legend.svelte
@@ -26,7 +26,7 @@
 				'd',
 				'M21,0H3A3,3,0,0,0,0,3V21a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V3A3,3,0,0,0,21,0ZM3,2H21a1,1,0,0,1,1,1V15.86L14.18,9.35a5.06,5.06,0,0,0-6.39-.06L2,13.92V3A1,1,0,0,1,3,2ZM21,22H3a1,1,0,0,1-1-1V16.48l7-5.63a3.06,3.06,0,0,1,3.86,0L22,18.47V21A1,1,0,0,1,21,22Z'
 			);
-			
+
 			iconPath.setAttribute('stroke-linecap', 'round');
 			iconPath.setAttribute('stroke-linejoin', 'round');
 			iconPath.setAttribute('stroke-width', '2');
@@ -98,7 +98,7 @@
 
 <style>
 	.legend {
-		display:inline-flex;
+		display: inline-flex;
 		height: 24px;
 		width: 24px;
 	}
