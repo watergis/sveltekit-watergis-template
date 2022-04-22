1c941f58fefca137da11b3ad8d0f5236a712c80b
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-22 20:24:25 +0100

    created zoomtoarea control

--- src/components/SearchPanel.svelte
+++ src/components/SearchPanel.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-	import Select, { Option } from '@smui/select';
-	import Icon from '@smui/select/icon';
-	import { config } from '../config';
-	import { map } from '../stores';
 	import SearchControl from './SearchControl.svelte';
-
+	import ZoomToAreaControl from './ZoomToAreaControl.svelte';
 	export let isSearchTabVisible = false;
-	let value = '';
-	$: areas = config.areaSwitcher.areas;
-	$: {
-		if ($map && value !== '') {
-			const selected = JSON.parse(value);
-			$map.flyTo({ center: selected.latlng, zoom: selected.zoom, curve: 1 });
-		}
-	}
 </script>
 
 {#if isSearchTabVisible}
-	<div class="select">
-		<p>Search area of interest</p>
-		<Select variant="outlined" bind:value label="Area">
-			<Icon class="material-icons" slot="leadingIcon">zoom_in_map</Icon>
-			<Option value="" />
-			{#each areas as areas}
-				<Option value={JSON.stringify(areas)}>{areas.title}</Option>
-			{/each}
-			<svelte:fragment slot="helperText">Select the area zooming to.</svelte:fragment>
-		</Select>
-	</div>
-
+	<ZoomToAreaControl />
 	<SearchControl />
 {/if}
 
