452d78f9352bb651f2985cd4c2fe7c4d5f569764
Jin Igarashi <igarashi.jin@water-gis.com>
2022-04-27 21:53:25 +0100

    deleted __layout and moved it to index.svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+	import DrawerContent from '../components/DrawerContent.svelte';
+	import Map from '../components/Map.svelte';
+	import { selectedStyle } from '../stores';
+	import { config } from '../config';
+
+	let drawerOpen = false;
+
+	const defaultStyle = config.styles[0];
+	selectedStyle.update(() => defaultStyle);
+</script>
+
+<Header bind:drawerOpen />
+<DrawerContent bind:open={drawerOpen}>
+	<Map />
+</DrawerContent>
