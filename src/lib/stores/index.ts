acd6c0917a9abb126da80d4390220961f53694f5
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-25 18:18:26 +0100

    migrade layer legend to svelte-maplibre-legend component

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -6,8 +6,6 @@ import { writable } from 'svelte/store';
 // map store for maplibre-gl object
 export const map = writable<Map>(null);
 
-export const spriteLoaderObject = writable(null);
-
 export const selectedStyle = writable<StyleSwitcherOption>(null);
 
 export const queriedFeatures = writable<MapGeoJSONFeature[]>(null);
