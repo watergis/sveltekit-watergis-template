85b40234b152d126245779a3287c8f62a618b595
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-16 23:13:39 +0100

    use typescript for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { resolve } from 'path';
+import type { UserConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
+const config: UserConfig = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
