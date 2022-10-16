3bcf065fb1b972a6ce21dcf109f8ef93d0a52846
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-16 10:41:30 +0100

    migrated smui to bulma for export (#23)

--- src/lib/components/MapExport.svelte
+++ src/lib/components/MapExport.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import IconButton from '@smui/icon-button';
-	import Button, { Label, Icon as ButtonIcon } from '@smui/button';
-	import Select, { Option } from '@smui/select';
-	import Icon from '@smui/select/icon';
-	import Dialog, { Title, Content, Actions } from '@smui/dialog';
 	import { PageOrientation, Size, DPI, Format, Unit } from '$lib/map-generator';
 	import type MapGenerator from '$lib/map-generator';
-	import PrintableAreaManager from '$lib/printable-area-manager';
+	// import PrintableAreaManager from '$lib/printable-area-manager';
 	import { map } from '$lib/stores';
 
 	let mapGenerator: MapGenerator;
-	let printableArea;
+	// let printableArea;
 	let paperSize = Size.A4;
 	let dpi = DPI[96];
 	let format = Format.PNG;
 	let orientation = PageOrientation.Landscape;
 	let isDialogOpen = false;
 
-	$: paperSize, updatePrintableArea();
-	$: orientation, updatePrintableArea();
+	// $: paperSize, updatePrintableArea();
+	// $: orientation, updatePrintableArea();
 
-	$: {
-		if (!isDialogOpen) {
-			togglePrintableArea(false);
-		}
-	}
+	// $: {
+	// 	if (!isDialogOpen) {
+	// 		togglePrintableArea(false);
+	// 	}
+	// }
 
 	onMount(async () => {
 		const { default: MapGenerator } = await import('../map-generator');
@@ -51,103 +47,140 @@
 			format,
 			Unit.mm
 		);
-		togglePrintableArea(false);
-		isDialogOpen = false;
+		handleClose();
 	};
 	const openMenu = () => {
 		isDialogOpen = true;
-		togglePrintableArea(true);
+		// togglePrintableArea(true);
 	};
 
-	const togglePrintableArea = (state: boolean) => {
-		if (state === false) {
-			if (printableArea !== undefined) {
-				printableArea.destroy();
-				printableArea = undefined;
-			}
-		} else {
-			printableArea = new PrintableAreaManager($map);
-			updatePrintableArea();
-		}
-	};
+	// const togglePrintableArea = (state: boolean) => {
+	// 	if (state === false) {
+	// 		if (printableArea !== undefined) {
+	// 			printableArea.destroy();
+	// 			printableArea = undefined;
+	// 		}
+	// 	} else {
+	// 		printableArea = new PrintableAreaManager($map);
+	// 		updatePrintableArea();
+	// 	}
+	// };
 
-	const updatePrintableArea = () => {
-		if (printableArea === undefined) {
-			return;
-		}
-		const actualPaperSize = getActualPaperSize();
-		printableArea.updateArea(actualPaperSize[0], actualPaperSize[1]);
-	};
+	// const updatePrintableArea = () => {
+	// 	if (printableArea === undefined) {
+	// 		return;
+	// 	}
+	// 	const actualPaperSize = getActualPaperSize();
+	// 	printableArea.updateArea(actualPaperSize[0], actualPaperSize[1]);
+	// };
 
-	const closeDialog = () => {
-		togglePrintableArea(false);
+	const handleClose = () => {
+		// togglePrintableArea(false);
 		isDialogOpen = false;
 	};
 </script>
 
 <IconButton class="material-icons" aria-label="Print" on:click={openMenu}>print</IconButton>
 
-<Dialog
-	bind:open={isDialogOpen}
-	aria-labelledby="mandatory-title"
-	aria-describedby="mandatory-content"
->
-	<Title id="mandatory-title">Export</Title>
-	<Content id="mandatory-content">
-		<div style="margin: 1em; display: flex; flex-direction: column; align-items: flex-end;">
-			<Select bind:value={paperSize} label="Paper Size">
-				<Icon class="material-icons" slot="leadingIcon">description</Icon>
-				{#each Object.keys(Size) as key}
-					<Option value={Size[key]}>{key}</Option>
-				{/each}
-			</Select>
-			<Select bind:value={orientation} label="Page Orientation">
-				<Icon class="material-icons" slot="leadingIcon">screen_rotation</Icon>
-				{#each Object.keys(PageOrientation) as key}
-					<Option value={PageOrientation[key]}>{key}</Option>
-				{/each}
-			</Select>
-			<Select bind:value={format} label="Format">
-				<Icon class="material-icons" slot="leadingIcon">picture_as_pdf</Icon>
-				{#each Object.keys(Format) as key}
-					<Option value={Format[key]}>{key}</Option>
-				{/each}
-			</Select>
-			<Select bind:value={dpi} label="DPI">
-				<Icon class="material-icons" slot="leadingIcon">high_quality</Icon>
-				{#each Object.keys(DPI) as key}
-					<Option value={DPI[key]}>{key}</Option>
-				{/each}
-			</Select>
-		</div>
-	</Content>
-	<div style="margin: 1em; display: flex; flex-direction:row; align-items: flex-end;">
-		<Actions>
-			<Button
-				style="margin-top: 1em;"
-				on:click={() => {
-					closeDialog();
-				}}
-				variant="raised"
-			>
-				<ButtonIcon class="material-icons">cancel</ButtonIcon>
-				<Label>Cancel</Label>
-			</Button>
-		</Actions>
-		<Actions>
-			<Button
-				style="margin-top: 1em;"
-				on:click={() => {
-					exportMap();
-				}}
-				variant="raised"
-			>
-				<ButtonIcon class="material-icons">file_download</ButtonIcon>
-				<Label>Export</Label>
-			</Button>
-		</Actions>
+<div class="modal {`${isDialogOpen ? 'is-active' : ''}`}">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="modal-background" on:click={handleClose} />
+	<div class="modal-card">
+		<header class="modal-card-head">
+			<p class="modal-card-title">Export</p>
+			<button class="delete" aria-label="close" on:click={handleClose} />
+		</header>
+		<section class="modal-card-body">
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">Paper Size</label>
+				<div class="control has-icons-left">
+					<div class="select is-fullwidth">
+						<select bind:value={paperSize}>
+							{#each Object.keys(Size) as key}
+								<option value={Size[key]}>{key}</option>
+							{/each}
+						</select>
+					</div>
+					<div class="icon is-small is-left">
+						<i class="fas fa-file" />
+					</div>
+				</div>
+			</div>
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">Page Orientation</label>
+				<div class="control">
+					{#each Object.keys(PageOrientation) as key}
+						<label class="radio" style="color:black">
+							<input
+								type="radio"
+								name="orientation"
+								on:click={() => {
+									orientation = PageOrientation[key];
+								}}
+								checked={orientation === PageOrientation[key]}
+							/>
+							<div class="icon is-small is-left">
+								<i
+									class="fas {`${
+										PageOrientation[key] === PageOrientation.Landscape
+											? 'fa-left-right'
+											: 'fa-up-down'
+									}`}"
+								/>
+							</div>
+							{key}
+						</label>
+					{/each}
+				</div>
+			</div>
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">Format</label>
+				<div class="control has-icons-left">
+					<div class="select is-fullwidth">
+						<select bind:value={format}>
+							{#each Object.keys(Format) as key}
+								<option value={Format[key]}>{key}</option>
+							{/each}
+						</select>
+					</div>
+					<div class="icon is-small is-left">
+						<i class="fas fa-file-pdf" />
+					</div>
+				</div>
+			</div>
+			<div class="field">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label class="label">DPI</label>
+				<div class="control has-icons-left">
+					<div class="select is-fullwidth">
+						<select bind:value={dpi}>
+							{#each Object.keys(DPI) as key}
+								<option value={DPI[key]}>{key}</option>
+							{/each}
+						</select>
+					</div>
+					<div class="icon is-small is-left">
+						<i class="fas fa-braille" />
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="modal-card-foot">
+			<button class="button" on:click={handleClose}>
+				<span class="icon">
+					<i class="fas fa-xmark" />
+				</span>
+				<span>Cancel</span>
+			</button>
+			<button class="button is-success" on:click={exportMap}>
+				<span class="icon">
+					<i class="fas fa-download" />
+				</span>
+				<span>Export</span>
+			</button>
+		</footer>
 	</div>
-</Dialog>
-
-<style>
-</style>
+</div>
