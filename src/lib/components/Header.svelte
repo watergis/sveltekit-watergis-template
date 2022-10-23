b3741da106aac0bdaf45aafb95625d9e4d0e3aac
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-23 13:11:37 +0100

    removed LightDarkSwitch svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,11 +4,11 @@
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
 	import { config } from '$config';
 	import ShareURL from './ShareURL.svelte';
-	import LightDarkSwitch from './LightDarkSwitch.svelte';
 	import MenuButton from './MenuButton.svelte';
 
 	export let drawerOpen = false;
 
+	let darkTheme: boolean;
 	let topAppBar: TopAppBarComponentDev;
 
 	let innerWidth = 0;
@@ -18,6 +18,7 @@
 
 	const setIsMobile = () => {
 		isMobile = window.matchMedia('only screen and (max-width: 768px)').matches;
+		darkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	};
 
 	onMount(() => {
@@ -30,6 +31,26 @@
 </script>
 
 <svelte:window bind:innerWidth />
+
+<svelte:head>
+	{#if darkTheme === undefined}
+		<link
+			rel="stylesheet"
+			href="{config.basePath}/smui.css"
+			media="(prefers-color-scheme: light)"
+		/>
+		<link
+			rel="stylesheet"
+			href="{config.basePath}/smui-dark.css"
+			media="screen and (prefers-color-scheme: dark)"
+		/>
+	{:else if darkTheme}
+		<link rel="stylesheet" href="{config.basePath}/smui.css" media="print" />
+		<link rel="stylesheet" href="{config.basePath}/smui-dark.css" media="screen" />
+	{:else}
+		<link rel="stylesheet" href="{config.basePath}/smui.css" />
+	{/if}
+</svelte:head>
 
 <TopAppBar bind:this={topAppBar} variant="fixed">
 	<Row>
@@ -45,7 +66,6 @@
 		<section />
 		<Section align="end" toolbar>
 			<ShareURL />
-			<LightDarkSwitch on:change={toggleMode} />
 		</Section>
 	</Row>
 </TopAppBar>
