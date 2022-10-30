5e56137f234b4830b275329a7518c36ee4246740
Jin Igarashi <igarashi.jin@water-gis.com>
2022-10-30 14:45:24 +0000

    added svelte-maplibre-valhalla (#41)

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,13 +1,8 @@
-import type ValhallaIsochrone from '$lib/valhalla-isochrone';
 import type { StyleSwitcherOption } from '@watergis/svelte-maplibre-style-switcher';
-import type { Map, LngLat } from 'maplibre-gl';
+import type { Map } from 'maplibre-gl';
 import { writable } from 'svelte/store';
 
 // map store for maplibre-gl object
 export const map = writable<Map>(null);
 
 export const selectedStyle = writable<StyleSwitcherOption>(null);
-
-export const valhallaControlData = writable<ValhallaIsochrone>(null);
-
-export const valhallaRoutingData = writable<LngLat[]>(null);
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,8 +1,1 @@
 export type { Config } from './config';
-export type {
-	ValhallaTripResult,
-	ValhallaRoutingLeg,
-	ValhallaRoutingLocation,
-	ValhallaTripSummary,
-	ValhallaError
-} from './valhallaRouting';
