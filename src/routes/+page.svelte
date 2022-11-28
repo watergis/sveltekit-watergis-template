fe4839972e3b5d676924bb26ea27351efc56af97
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-28 09:44:27 +0000

    created Netatag,svekte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,30 +2,17 @@
 	import Map from '$lib/components/Map.svelte';
 	import { selectedStyle } from '$lib/stores';
 	import { config } from '$config';
+	import Metatag from '$lib/components/Metatag.svelte';
 
 	const defaultStyle = config.styles[0];
 	selectedStyle.update(() => defaultStyle);
 </script>
 
-<svelte:head>
-	<!-- Primary Meta Tags -->
-	<title>{config.title}</title>
-	<meta name="title" content={config.title} />
-	<meta name="description" content={config.description} />
-
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content={config.site_url} />
-	<meta property="og:title" content={config.title} />
-	<meta property="og:description" content={config.description} />
-	<meta property="og:image" content={config.preview} />
-
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content={config.site_url} />
-	<meta property="twitter:title" content={config.title} />
-	<meta property="twitter:description" content={config.description} />
-	<meta property="twitter:image" content={config.preview} />
-</svelte:head>
+<Metatag
+	bind:title={config.title}
+	bind:description={config.description}
+	bind:site_url={config.site_url}
+	bind:preview={config.preview}
+/>
 
 <Map />
