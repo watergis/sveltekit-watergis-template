2ebce9049117f2ef138638d3aaab9a309ed262c5
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-04 10:58:53 +0000

    updated measure package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { resolve } from 'path';
 import type { UserConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
 			$config: resolve('./src/config.ts')
+		}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true
+				})
+			]
 		}
 	}
 };
