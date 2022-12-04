c2be6b29cd6275afabeee1f056b8c60f91227828
Jin Igarashi <igarashi.jin@water-gis.com>
2022-12-04 11:39:25 +0000

    Update measure package (#52)
    * updated measure package
    
    * updated pnpm
    
    * fixed buffer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,16 @@
 	import Map from '$lib/components/Map.svelte';
 	import { selectedStyle } from '$lib/stores';
 	import { config } from '$config';
+	import { onMount } from 'svelte';
 
 	const defaultStyle = config.styles[0];
 	selectedStyle.update(() => defaultStyle);
+
+	onMount(async () => {
+		window.global = window;
+		const Buffer = await (await import('buffer')).Buffer;
+		window.Buffer = Buffer;
+	});
 </script>
 
 <Map />
