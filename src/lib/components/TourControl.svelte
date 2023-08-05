f58b8820a898b48bbbc165db3a9e9d199ff13d6a
Jin Igarashi <igarashi.jin@water-gis.com>
2023-08-05 22:32:43 +0100

    fix: because completeOnFinish does not work in incognito window, implemented localstorage by myself to store state

--- src/lib/components/TourControl.svelte
+++ src/lib/components/TourControl.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import type { Map, ControlPosition } from 'maplibre-gl';
-	import '@sjmc11/tourguidejs/src/scss/tour.scss';
+	import '@sjmc11/tourguidejs/src/scss/tour.scss'; // Styles
 	import { TourGuideClient, type TourGuideOptions } from '@sjmc11/tourguidejs';
 
 	export let map: Map;
 	export let position: ControlPosition = 'top-right';
+	// see the documentation of TourGuide JS
+	// https://tourguidejs.com/docs/options.html
 	export let tourguideOptions: TourGuideOptions;
-	export let group = 'maptour';
+	// change LocalStorageKey for your app.
+	export let localStorageKey = 'tourGuideFinished';
+
+	// configulation of localstorage to store the state of tour completion
+	const initialValue = browser
+		? window.localStorage.getItem(localStorageKey) === 'true'
+			? true
+			: false
+		: false;
+	const tourGuideFinished = writable<boolean>(initialValue);
+	tourGuideFinished.subscribe((value) => {
+		if (browser) {
+			window.localStorage.setItem(localStorageKey, value ? 'true' : 'false');
+		}
+	});
 
 	const tourguide = writable<TourGuideClient>(null);
 
@@ -32,10 +49,13 @@
 			const tg = new TourGuideClient(tourguideOptions);
 			tourguide.update(() => tg);
 		}
-		const isFinished = $tourguide.isFinished(group);
-		console.log(isFinished);
-		if (!isFinished) {
-			$tourguide.start(group);
+
+		if (!$tourGuideFinished) {
+			$tourguide.start();
+
+			$tourguide.onFinish(() => {
+				$tourGuideFinished = true;
+			});
 		}
 
 		return this.controlContainer;
@@ -51,11 +71,11 @@
 	};
 
 	TourControl.prototype.tourStart = () => {
-		$tourguide?.start(group);
+		$tourguide?.start();
 
-		// $tourguide?.onFinish(() => {
-		// 	$tourGuideFinished = true;
-		// });
+		$tourguide?.onFinish(() => {
+			$tourGuideFinished = true;
+		});
 	};
 
 	/*global TourControl */
@@ -80,7 +100,6 @@
 </script>
 
 <style lang="scss">
-	/* a style for terrain rgb detector button */
 	:global(.maplibre-tour-icon) {
 		background: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>');
 		background-position: center;
