0411561b4e5d6005a4802b0a07d91d5b5ed6cd27
Jin Igarashi <igarashi.jin@water-gis.com>
2023-09-28 21:45:37 +0100

    updated menu/attribute-table plugins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,4 +14,18 @@
 	});
 </script>
 
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+		integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+		crossorigin="anonymous"
+		referrerpolicy="no-referrer"
+	/>
+</svelte:head>
+
 <Map />
+
+<style global lang="scss">
+	@import 'bulma/bulma.sass';
+</style>
